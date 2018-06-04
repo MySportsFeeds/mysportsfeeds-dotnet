8d3f0957f9ea7b6048266946f2707f01bb021e05
Joe Arnone <arnonejoe@gmail.com>
2018-06-03 22:29:51 -0700

    wired in division team standings and daily player stats

---
+++ MySportsFeeds.NetCore/MySportsFeeds.NetCore/Models/DivisionTeamStandingsResponse.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace MySportsFeeds.NetCore.Models
+{
+    public class DivisionTeamStandingsResponse
+    {
+        [JsonProperty("divisionteamstandings")]
+        public Divisionteamstandings DivisionTeamStandings { get; set; }
+    }
+    
+    public class DivisionTeamStandingStats
+    {
+        public GamesPlayed GamesPlayed { get; set; }
+        public Wins Wins { get; set; }
+        public Losses Losses { get; set; }
+        public RunsFor RunsFor { get; set; }
+        public RunsAgainst RunsAgainst { get; set; }
+    }
+
+    public class DivisonTeamEntry
+    {
+        [JsonProperty("team")]
+        public Team Team { get; set; }
+
+        [JsonProperty("rank")]
+        public string Rank { get; set; }
+
+        [JsonProperty("stats")]
+        public DivisionTeamStandingStats Stats { get; set; }
+    }
+
+    public class Division
+    {
+        [JsonProperty("@name")]
+        public string Name { get; set; }
+
+        [JsonProperty("teamentry")]
+        public List<DivisonTeamEntry> TeamEntry { get; set; }
+    }
+
+    public class Divisionteamstandings
+    {
+        [JsonProperty("lastUpdatedOn")]
+        public string LastUpdatedOn { get; set; }
+
+        [JsonProperty("division")]
+        public List<Division> Division { get; set; }
+    }
+}
