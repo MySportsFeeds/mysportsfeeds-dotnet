396a2306bf7c9d4eba29c600ce551e907a8e7c6f
Joe Arnone <arnonejoe@gmail.com>
2018-06-09 18:59:09 -0700

    added game box score and game starting lineup

--- MySportsFeeds.NetCore/MySportsFeeds.NetCore.Tests/MySportsFeedsClientTests.cs
+++ MySportsFeeds.NetCore/MySportsFeeds.NetCore.Tests/MySportsFeedsClientTests.cs
@@ -376,5 +376,57 @@ namespace MySportsFeeds.NetCore.IntegrationTests
 
             Assert.NotNull(response);
         }
+
+        [Fact]
+        public async Task Can_Get_Game_Box_Score()
+        {
+            // Arrange
+
+            string[] teamStatsOptions = new string[4];
+            teamStatsOptions[0] = "W";
+            teamStatsOptions[1] = "L";
+            teamStatsOptions[2] = "RF";
+            teamStatsOptions[3] = "RA";
+            
+            string[] playerStatsOptions = new string[4];
+            playerStatsOptions[0] = "AB";
+            playerStatsOptions[1] = "H";
+            playerStatsOptions[2] = "HR";
+            playerStatsOptions[3] = "ER";
+            
+            var requestOptions = new Helpers.RequestOptions()
+            {
+                GameId = GAME_ID,
+                TeamStatsMlb = teamStatsOptions,
+                PlayerStatsMlb = playerStatsOptions
+            };
+
+            // Act
+
+            var response = await mySportsFeedsClient.GameBoxScore.Get(League.MLB, 2018, SeasonType.Regular, requestOptions);
+
+            // Assert
+
+            Assert.NotNull(response);
+        }
+
+        [Fact]
+        public async Task Can_Get_Game_Starting_Lineup()
+        {
+            // Arrange
+
+            var requestOptions = new Helpers.RequestOptions()
+            {
+                GameId = GAME_ID
+            };
+
+            // Act
+
+            var response = await mySportsFeedsClient.GameStartingLineup.Get(League.MLB, 2018, SeasonType.Regular, requestOptions);
+
+            // Assert
+
+            Assert.NotNull(response);
+        }
     }
 }
